b89906ee68a330372bf86a8fcc416fb4d0512ad4
masaki0114 <masaki0114likebs@icloud.com>
2020-09-08 20:45:16 +0900

    main.pyで複数の評価を可能にした

--- main.py
+++ main.py
@@ -2,29 +2,39 @@ from keras import models
 from keras.models import model_from_json
 from keras.preprocessing import image
 import numpy as np
+import os
+import glob
+import pathlib
 
 # 保存したモデルの読み込み
 model = model_from_json(open("model/hogarth_model.json").read())
 # 保存した重みの読み込み
 model.load_weights("model/hogarth_weight.hdf5")
 
-categories = ["hogarth-curve","another"]
+categories = ["hula","india","jawa","myan","nichibu"]
 
-# 画像を読み込み
-print("判断する画像を指定してください")
-number = str(input())
-img_path = "unknown-deta/" + number + ".jpeg" # 未知データの画像を指定
-img = image.load_img(img_path,target_size=(250,250,3))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-# ここでのxは画像の行列(255. 255. 255)のようなRGB配列
-# 予測する
-features = model.predict(x)
-print(features) #onehot表現になっている
+#出力ディレクトリの設定
+for x in range(len(categories)):
+	output_dir = os.path.join(".","R","ans",categories[x])
+	if not(os.path.exists(output_dir)):
+		os.mkdir(output_dir)
+	#画像の読み込み
+	images = glob.glob(os.path.join(".","R",categories[x],"*.png"))
+	# print(images)
 
-# 条件分岐
-if features[0,0] == 1 :
-	print("ホガースカーブです")
+	with open(os.path.join(output_dir,"ans.txt"),mode="w") as f:
+		for y in range(len(images)):
+			l = []
+			img_path = images[y]
+			img = image.load_img(img_path,target_size=(250,250,3))
+			img = image.img_to_array(img)
+			img = np.expand_dims(img, axis=0)
+			features = model.predict(img)	#featuresは２次元配列
+			for z in range(7):
+				l.append(features[0][z])
+			f.write(img_path+'\n')
+			l = [str(i) for i in l]
+			f.write('\n'.join(l) + '\n')
+			message = '最も近いのは'+str(np.argmax(features)+1)+'番目のホガースカーブ'
+			f.write(message + '\n')
 
-else:
-	print("ホガースカーブではありません")
